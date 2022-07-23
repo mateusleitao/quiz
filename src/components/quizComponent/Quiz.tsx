7c35069eb05e23876729b8d5b500bc5fdbc8fc2a
mateus <mateuselpidio0@gmail.com>
2022-07-23 16:27:46 -0300

    style partially finished, need to do some improvements like responsivity and effects

--- src/components/quizComponent/Quiz.tsx
+++ src/components/quizComponent/Quiz.tsx
@@ -1,12 +1,25 @@
-import React from 'react'
-import "./quiz.sass"
+import React from "react";
+import "./quiz.sass";
+import cardImg from "../../assets/progCard.png"
 
-type Props = {}
+type Props = {
+
+};
 
 const Quiz = (props: Props) => {
   return (
-    <div className='container'>Quiz</div>
-  )
-}
+    <div className="container">
+      <div className="quiz-grid">
+        <div className="card">
+          <img src={cardImg}alt="cardImage" width="900px"/>
+          <h2 className="cardTitle">Web Development</h2>
+          <p></p>
+          <a href="#" className="answerButton">Answer</a>
+        </div>
+        
+      </div>
+    </div>
+  );
+};
 
-export default Quiz
+export default Quiz;
---
+++ src/index.d.ts
@@ -0,0 +1,1 @@
+declare module "./assets/progCard.png"
