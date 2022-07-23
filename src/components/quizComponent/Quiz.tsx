5a6f4919b81d6de10e8684a7e9da1426ce3b741c
mateus <mateuselpidio0@gmail.com>
2022-07-23 14:09:49 -0300

    quiz component created and main container style finished

---
+++ src/components/quizComponent/Quiz.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./quiz.sass"
+
+type Props = {}
+
+const Quiz = (props: Props) => {
+  return (
+    <div className='container'>Quiz</div>
+  )
+}
+
+export default Quiz
